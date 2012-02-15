035d49ed6f6ade0fbe86b09b503232080208b93d
bastianonm <bastianonm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-15 16:04:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-gpx-maps/tags/1.1.6@505497 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-gpx-maps_admin.php
@@ -0,0 +1,126 @@
+<?php
+
+if ( is_admin() ){
+
+	add_action('admin_menu', 'wpgpxmaps_admin_menu');
+
+	function wpgpxmaps_admin_menu() {
+		add_options_page('WP GPX Maps', 'WP GPX Maps', 'administrator', 'WP-GPX-Maps', 'WP_GPX_Maps_html_page');
+	}
+}
+
+function ilc_admin_tabs( $current  ) {
+    $tabs = array( 'tracks' => 'Tracks', 'settings' => 'Settings', 'help' => "help" );
+    echo '<h2 class="nav-tab-wrapper">';
+    foreach( $tabs as $tab => $name ){
+        $class = ( $tab == $current ) ? ' nav-tab-active' : '';
+        echo "<a class='nav-tab$class' href='?page=WP-GPX-Maps&tab=$tab'>$name</a>";
+    }
+    echo '</h2>';
+}
+
+function WP_GPX_Maps_html_page() {
+	$realGpxPath = gpxFolderPath();
+	$cacheGpxPath = gpxCacheFolderPath();
+	$relativeGpxPath = relativeGpxFolderPath();
+	$relativeGpxPath = str_replace("\\","/", $relativeGpxPath);
+	$gpxRegEx = '/.gpx$/';
+	
+	$tab = $_GET['tab'];
+	
+	if ($tab == '')
+		$tab = 'tracks';
+	
+
+?>
+	<div id="icon-themes" class="icon32"><br></div>
+		<h2>WP GPX Settings</h2>	
+<?php
+
+	if(file_exists($realGpxPath) && is_dir($realGpxPath))
+	{
+		//dir exsist!
+	}
+	else
+	{
+		if (!@mkdir($realGpxPath,0755,true)) {
+			echo '<div class="error" style="padding:10px">
+					Can\'t create <b>'.$realGpxPath.'</b> folder. Please create it and make it writable!<br />
+					If not, you will must update the file manually!
+				  </div>';
+		}
+	}
+	
+	if(file_exists($cacheGpxPath) && is_dir($cacheGpxPath))
+	{
+		//dir exsist!
+	}
+	else
+	{
+		if (!@mkdir($cacheGpxPath,0755,true)) {
+			echo '<div class="error" style="padding:10px">
+					Can\'t create <b>'.$cacheGpxPath.'</b> folder. Please create it and make it writable!<br />
+					If not, cache will not created and your site could be slower!
+				  </div>';
+		}
+	}
+
+	ilc_admin_tabs($tab);	
+	
+	if ($tab == "tracks")
+	{
+		include 'wp-gpx-maps_admin_tracks.php';
+	}
+	else if ($tab == "settings")
+	{
+		include 'wp-gpx-maps_admin_settings.php';
+	}
+	else if ($tab == "help")
+	{
+?>
+
+	<div style="padding:10px;">
+		<b>The fastest way to use this plugin:</b><br /> upload the file using the uploader in the first tab, than copy the shortcode from the list and paste it in the pages/posts.
+		<p>You can manually set the relative path to your gpx: <b>[sgpx gpx="<?php echo $relativeGpxPath; ?>&lt gpx file name &gt"]</b>.</p>
+		<p>You can also use gpx from other sites: <b>[sgpx gpx="http://www.someone.com/somewhere/somefile.gpx"]</b></p>
+		<hr />
+		<p>
+			<i>Full set of attributes:</i> <b>[sgpx 
+													gpx="<?php echo $relativeGpxPath; ?>&lt gpx file name &gt" 
+													width=100% 
+													mheight=450px 
+													gheight=200px 
+													mtype=SATELLITE 
+													waypoints=true 
+													donotreducegpx=false 
+													pointsoffset=10
+													uom=0
+													mlinecolor=#3366cc
+													glinecolor=#3366cc]</b>
+
+			<ul>
+				<li><b>gpx</b>: relative path to gpx</li>
+				<li><b>width</b>: width in pixels</li>
+				<li><b>mheight</b>: map height</li>
+				<li><b>gheight</b>: graph height</li>
+				<li><b>mtype</b>: map aviable types are: HYBRID, ROADMAP, SATELLITE, TERRAIN</li>
+				<li><b>waypoints</b>: print the gpx waypoints inside the map (default is FALSE)</li>
+				<li><b>donotreducegpx</b>: Print all the point without reduce it (default is FALSE)</li>
+				<li><b>pointsoffset</b>: Skip points closer than XX meters(default is 10)</li>
+				<li><b>uom</b>: the unit of measure values are: 0, 1 (0 = meters, 1 = miles/feet)</li>
+				<li><b>mlinecolor</b>: map line color (default is #3366cc)</li>
+				<li><b>glinecolor</b>: graph line color (default is #3366cc)</li>
+			</ul>
+
+			<p>
+				<a href="http://www.darwinner.it/featured/wp-gpx-maps/">Bugs, problems, thanks and anything else here!</a>
+			</p>
+			
+		</p>
+	</div>
+
+<?php
+	}
+
+}
+?>
---
+++ wp-gpx-maps_admin_settings.php
@@ -0,0 +1,141 @@
+
+<script type="text/javascript" src="http://meta100.github.com/mColorPicker/javascripts/mColorPicker_min.js" charset="UTF-8"></script>
+
+<?php
+	
+	$po = get_option('wpgpxmaps_pointsoffset');
+	$showW = get_option("wpgpxmaps_show_waypoint");	
+	$donotreducegpx = get_option("wpgpxmaps_donotreducegpx");
+	$t = get_option('wpgpxmaps_map_type');
+	$uom = get_option('wpgpxmaps_unit_of_measure');
+	$uomspeed = get_option('wpgpxmaps_unit_of_measure_speed');
+	
+	if (!($t))
+		$t = 'HYBRID';
+	if (!($po))
+		$po = 10;
+		
+?>
+
+<form method="post" action="options.php">
+
+	<?php wp_nonce_field('update-options') ?>
+	
+	
+	<h3 class="title">Map and Chart size</h3>
+	
+	<table class="form-table">
+		<tr>
+			<th scope="row">Width:</th>
+			<td>
+				<input name="wpgpxmaps_width" type="text" id="wpgpxmaps_width" value="<?php echo get_option('wpgpxmaps_width'); ?>" style="width:50px;" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Map Height:</th>
+			<td>
+				<input name="wpgpxmaps_height" type="text" id="wpgpxmaps_height" value="<?php echo get_option('wpgpxmaps_height'); ?>" style="width:50px;" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Graph Height:</th>
+			<td>
+				<input name="wpgpxmaps_graph_height" type="text" id="wpgpxmaps_graph_height" value="<?php echo get_option('wpgpxmaps_graph_height'); ?>" style="width:50px;" />
+			</td>
+		</tr>
+	</table>
+	
+	<h3 class="title">Maps</h3>
+	
+	<table class="form-table">
+		<tr>
+			<th scope="row">Waypoints Support:</th>
+			<td>
+				<input name="wpgpxmaps_show_waypoint" type="checkbox" value="true" <?php if($showW == true){echo('checked');} ?> onchange="this.value = (this.checked)"  /><i>Show Waypoints</i>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Map line color:</th>
+			<td>
+				<input name="wpgpxmaps_map_line_color" type="color" data-hex="true" value="<?php echo get_option('wpgpxmaps_map_line_color'); ?>" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Default Map Type:</th>
+			<td>
+				<input type="radio" name="wpgpxmaps_map_type" value="HYBRID" <?php if ($t == 'HYBRID') echo 'checked'; ?> > HYBRID: transparent layer of major streets on satellite images.<br />
+				<input type="radio" name="wpgpxmaps_map_type" value="ROADMAP" <?php if ($t == 'ROADMAP') echo 'checked'; ?>> ROADMAP: normal street map.<br />
+				<input type="radio" name="wpgpxmaps_map_type" value="SATELLITE" <?php if ($t == 'SATELLITE') echo 'checked'; ?>> SATELLITE: satellite images.<br />
+				<input type="radio" name="wpgpxmaps_map_type" value="TERRAIN" <?php if ($t == 'TERRAIN') echo 'checked'; ?>> TERRAIN: maps with physical features such as terrain and vegetation.<br />
+			</td>
+		</tr>
+	</table>
+	
+	<h3 class="title">Chart</h3>
+	
+	<table class="form-table">
+		<tr>
+			<th scope="row">Unit of measure:</th>
+			<td>
+				<select name='wpgpxmaps_unit_of_measure'>
+					<option value="0" <?php if ($uom == '0') echo 'selected'; ?>>meters</option>
+					<option value="1" <?php if ($uom == '1') echo 'selected'; ?>>miles/feet</option>
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Elevation line color:</th>
+			<td>
+				<input name="wpgpxmaps_graph_line_color" type="color" data-hex="true" value="<?php echo get_option('wpgpxmaps_graph_line_color'); ?>" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Show speed:</th>
+			<td>
+				<input name="wpgpxmaps_show_speed" type="checkbox" value="true" <?php if($showW == true){echo('checked');} ?> onchange="this.value = (this.checked)"  /><i>Show Speed (where aviable)</i>
+			</td>
+		</tr>		
+		<tr>
+			<th scope="row">Speed line color:</th>
+			<td>
+				<input name="wpgpxmaps_graph_line_color_speed" type="color" data-hex="true" value="<?php echo get_option('wpgpxmaps_graph_line_color_speed'); ?>" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">Speed unit of measure:</th>
+			<td>
+				<select name='wpgpxmaps_unit_of_measure_speed'>
+					<option value="0" <?php if ($uomspeed == '0') echo 'selected'; ?>>m/s</option>
+					<option value="1" <?php if ($uomspeed == '1') echo 'selected'; ?>>km/h</option>
+					<option value="2" <?php if ($uomspeed == '2') echo 'selected'; ?>>miles/h</option>
+				</select>
+			</td>
+		</tr>
+	</table>
+	
+	<h3 class="title">Advanced options	<small>(Do not edit if you don't know what you are doing!)</small></h3>
+
+	
+	<table class="form-table">
+		<tr>
+			<th scope="row"></th>
+			<td>
+				<i>Skip points closer than </i> <input name="wpgpxmaps_pointsoffset" type="text" id="wpgpxmaps_pointsoffset" value="<?php echo $po ?>" style="width:50px;" /><i>meters</i>.
+			</td>
+		</tr>		
+		<tr>
+			<th scope="row"></th>
+			<td>
+				<input name="wpgpxmaps_donotreducegpx" type="checkbox" value="true" <?php if($donotreducegpx == true){echo('checked');} ?> onchange="this.value = (this.checked)"  /><i>Do not reduce gpx</i>.
+			</td>
+		</tr>	
+	</table>
+
+	<input type="hidden" name="action" value="update" />
+	<input name="page_options" type="hidden" value="wpgpxmaps_map_type,wpgpxmaps_height,wpgpxmaps_graph_height,wpgpxmaps_width,wpgpxmaps_show_waypoint,wpgpxmaps_pointsoffset,wpgpxmaps_donotreducegpx,wpgpxmaps_unit_of_measure,wpgpxmaps_map_line_color,wpgpxmaps_graph_line_color,wpgpxmaps_show_speed,wpgpxmaps_graph_line_color_speed,wpgpxmaps_unit_of_measure_speed" />
+
+	<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+	</p>
+
+</form>
