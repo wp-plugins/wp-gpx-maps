51f5e0d15f754f3f0824f099aa4919330bd6e335
bastianonm <bastianonm@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-25 07:44:15 +0000

    Fix display version
    git-svn-id: https://plugins.svn.wordpress.org/wp-gpx-maps/tags/1.3.7@1120275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-gpx-maps.php
+++ wp-gpx-maps.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-GPX-Maps
 Plugin URI: http://www.devfarm.it/
 Description: Draws a GPX track with altitude chart
-Version: 1.3.6
+Version: 1.3.7
 Author: Bastianon Massimo
 Author URI: http://www.pedemontanadelgrappa.it/
 */
